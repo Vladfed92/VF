6e8b8cf89fa1570e8da09d507b12a2252eee9058
Vladislav Fedorenko <107477959+Vladfed92@users.noreply.github.com>
2023-02-10 14:14:35 +0200

    made portfolio website

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,62 @@
+
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
+import Splide from '@splidejs/splide';
 
-//    BURGER JQERY    //
 
 $(document).ready(function () {
-	$('.burger').click(function (event) {
-		$('.burger,.burger__item,.navigation,.menu').toggleClass('active');
-	});
+	if ($('.about-me__splid')) {
+		new Splide('.about-me__splide', {
+			type: 'loop',
+			perPage: 1,
+			pagination: false,
+		}).mount();
+	};
+
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 0) {
+			$(".navigation").addClass('stiky');
+		}
+		else { $('.navigation').removeClass('stiky') }
+	})
+})
+
+
+// variables for togle 'actve' on burger
+const classesForActivationBurger = document.querySelectorAll('.burger,.burger__item,.navigation,.menu');
+const burger = document.querySelector('.burger');
+
+// add new class 'active' function
+const toggleClass = (element) => {
+	element.classList.toggle('active');
+};
+
+//togle class 'active' to 'burger' element
+burger.addEventListener('click', function () {
+	classesForActivationBurger.forEach(toggleClass)
 });
 
+
+
+const menuLinks = document.querySelectorAll('.menu__icon [data-goto]');
+
+const onMenuLinkClick = (element) => {
+	const menuLink = element.target;
+	if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+		const goToBlock = document.querySelector(menuLink.dataset.goto);
+		const goToBlockValue = goToBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+		window.scrollTo({
+			top: goToBlockValue,
+			behavior: "smooth",
+		});
+		element.preventDefault();
+	}
+}
+
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick)
+	})
+
+};
